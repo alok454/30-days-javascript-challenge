3e4fa1b1cc1f29eb3f0523c487c8c5857d01afd6
Alok Kumar <alokkumar82980@gmail.com>
2024-07-16 16:50:31 +0530

    day 2 challenge upload

---
+++ day-2/day-2.js
@@ -0,0 +1,111 @@
+// Activity 1 : Arithmetic Oprations
+
+
+
+let number1 = 50;
+let number2 = 4;
+
+// Task 1:
+let addResult = number1 + number2;
+console.log(addResult)  // 54
+
+// Task 2:
+let subtractResult = number1 - number2;
+console.log(subtractResult)  // 46
+
+// Task 3:
+let multiplyResult = number1 * number2;
+console.log(multiplyResult)  // 200
+ 
+// Task 4:
+let divideResult = number1 / number2;
+console.log(divideResult) // 12.5
+
+// Task 5:
+let remainderResult = number1 % number2;
+console.log(remainderResult) // 2
+
+
+// Activity 2: Assignment Operators
+
+// Task 6:
+let assignmentVariable = 39
+
+assignmentVariable += 5;
+console.log(assignmentVariable)  // 44
+
+assignmentVariable -= 6;
+console.log(assignmentVariable)  // 38
+
+
+// Activity 3: Comparison Operator
+
+let compVar1 = 83447;
+let compVar2 = 37482;
+
+// Task 8:
+console.log(compVar1 > compVar2)  // true
+console.log(compVar1 < compVar2)  // false
+
+// Task 9:
+console.log(compVar1 >= compVar2)  // true
+console.log(compVar1 <= compVar2)  // false
+ 
+// Task 10:
+let equalVar1 = 20;
+let equalVar2 = '20';
+console.log(equalVar1 == equalVar2)  // true
+console.log(equalVar1 === equalVar2)  // false
+
+
+// Activity 4 : Logical Operators
+
+let condition1 = 11 > 12;
+let condition2 = false < true
+
+// Task 11: 
+console.log(condition1 && condition2)  // false
+
+// Task 12:
+console.log(condition1 || condition2)  // true
+
+// Task 13:
+console.log( !condition1)  // true
+
+
+// Activity 5 : Ternary Operator
+
+// Task 14:
+let ternaryNumber = -15;
+let result = ternaryNumber > 0 ? 'Positive Number' : 'Negative Number';
+console.log(result)  // Negative Number
+
+
+
+// Feature Task
+
+// 1.
+let numberOne = 55;
+let numberTwo = 10;
+console.log(numberOne + numberTwo);  // 65
+console.log(numberOne - numberTwo);  // 45
+console.log(numberOne * numberTwo);  // 550
+console.log(numberOne / numberTwo);  // 5.5
+
+// 2.
+let comparisonOne = 50;
+let comparisonTwo = 60;
+console.log(comparisonOne > comparisonTwo)  // false
+console.log(comparisonOne < comparisonTwo)  // true
+console.log(comparisonOne >= comparisonTwo)  // false
+console.log(comparisonOne <= comparisonTwo)  // true
+console.log(comparisonOne == comparisonTwo)  // false
+console.log(comparisonOne === comparisonTwo)  // false
+
+console.log(comparisonOne > comparisonTwo && comparisonOne >= comparisonTwo)  // false
+console.log(comparisonOne > comparisonTwo || comparisonOne >= comparisonTwo)  // false
+console.log(!(comparisonOne > comparisonTwo))  // true
+
+// 3.
+const ternaryNum = 29;
+console.log(ternaryNum > 0 ? 'Positive' : 'Negative');  // Positive
